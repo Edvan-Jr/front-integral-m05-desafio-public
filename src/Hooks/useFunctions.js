e088e2845432d6ca9cf12be65f3b81ff34cb47c3
Edvan-Jr <edvan.hd@gmail.com>
2021-11-29 12:36:23 -0300

    Enxugando useFunctions

--- src/Hooks/useFunctions.js
+++ src/Hooks/useFunctions.js
@@ -1,15 +1,15 @@
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 import useConsumer from './useConsumer'
 require('dotenv').config()
 
 function useFunctions() {
-    const { tokenStorage, setTokenStorage, email, senha } = useConsumer()
+    const { /* tokenStorage, setTokenStorage, */ email, senha } = useConsumer()
 
     function signIn() {
-        const usuario = {
-            email,
-            senha
-        }
+        // const usuario = {
+        //     email,
+        //     senha
+        // }
         // const user = jwt.sign(usuario, process.env.JWT_SECURE)
         console.log(process.env.JWT_SECURE)
     }
